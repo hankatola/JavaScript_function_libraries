24d0d462a7d394affaef7f27da439f24cf58c4f4
Dave Paquette <30414198+hankatola@users.noreply.github.com>
2019-01-12 13:26:37 -0500

    create num.js & prime number generator
    Prime number generator using the 'sieve of eratosthenes'

---
+++ num.js
@@ -0,0 +1,26 @@
+function primes(n,all=false) {
+    let l = []
+    let z = []
+    let p = []
+    for (let i=3; i<=n; i=i+2) {
+        l.push(i)
+        z.push(0)
+    }
+    let c = 0
+    for (let i in l) {
+        for (let j=l[i]+c; j in l; j=j+l[i]) {
+            z[j] = 1
+        }
+        c++
+    }
+    for (let i in z) {
+        if (z[i] === 0) {p.push(l[i])}
+    }
+    if (n >= 2) {
+        p.unshift(2)
+    }
+    if (all===false) {
+        p = p[p.length - 1]
+    }
+    return p
+}
