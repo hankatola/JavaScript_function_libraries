4b07ac3076a6a45a0745aacca6967584709d5659
dPaq <dpaquette2.0@gmail.com>
2019-01-14 11:17:11 -0500

    round & binomial probability

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Contains math algorithms from my python work ported to JavaScript.
 
 Functions include:
+    round(n,p)
+        works like ms excel & python round()
     factorial(n)
         returns n!
     primes(n,all=true)
@@ -11,3 +13,7 @@ Functions include:
         returns combinations of size r from population n
     nPr(n,r)
         returns permuations of size r from population n
+    binomial(n,k,p,cumulative=false,get=false,precision=false)
+        returns prob of choosing 'k' out of 'n' population where prob of 'k' is p
+        cumulative available. Returns object unless get === ('p' || 'v' || 'avg')
+        precision defaults to 6 decimal places unless overriden.
--- num.js
+++ num.js
@@ -1,3 +1,8 @@
+function round(n,p) {
+    // like ms excel round
+    return Math.round(n * Math.pow(10,p)) / Math.pow(10,p)
+}
+
 function factorial(n) {
     // factorial function
     let x = 1
@@ -50,4 +55,33 @@ function nCr(n,r) {
 function nPr(n,r) {
     // returns permutations of size r from population n
     return factorial(n) / factorial(n-r)
+}
+
+function binomial(n,k,p,cumulative=false,get=false,precision=false) {
+    // choose k with probability p from population n
+    // precision defaults to 10^-6. Cumulative available
+    let q = 1 - p
+    let ans = {
+        p:0.0,
+        avg:0.0,
+        v:0.0
+    }
+    if (cumulative === true) {
+        ans.p = 0
+
+        for (let i = 0; i < k + 1; i++) {
+            ans.p += nCr(n,i) * Math.pow(p,i) * Math.pow(q, n-i)
+        }
+    }
+    else {
+        ans.p = nCr(n,k) * Math.pow(p,k) * Math.pow(q,n-k)
+    }
+    ans.avg = p * n
+    ans.v = ans.avg * q
+    if (precision === false) {precision = 6}
+    ans.p = round(ans.p,precision)
+    ans.avg = round(ans.avg,precision)
+    ans.v = round(ans.v,precision)
+    if (get) {ans = ans[get]}
+    return ans
 }
